349f286f308d205e1d6f4f4eb9e5cf22a8848798
Kevin Ng <102876076+KevinNg2@users.noreply.github.com>
2023-05-03 08:40:38 -0700

    add KN logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,33 +1,46 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-// import LogoS from '../../assets/images/logo-s.png'
-// import LogoSubtitle from '../../assets/images/logo_sub.png'
+// import LogoS from '../../assets/images/logo1.png'
+import LogoSubtitle from '../../assets/images/kn.initials.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBook, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons' //add faBars later from down below
+import { faBook, faEnvelope, faHome, faUser, faBars, faClose } from '@fortawesome/free-solid-svg-icons' //add faBars later from down below
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            {/* <img src={LogoS} alt="logo" /> */}
-            {/* <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faBook} color="4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
-            </NavLink>
-            
-        </nav>
-        <ul>
+      <Link className='logo' to='/'>
+        {/* <img src={LogoS} alt="Logo" /> */}
+        <img className="sub-logo" src={LogoSubtitle} alt="Kevin" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+        </NavLink>
+        <NavLink onClick={() => setShowNav(false)}
+          exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+        </NavLink>
+        <NavLink onClick={() => setShowNav(false)}
+          exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+          <FontAwesomeIcon icon={faBook} color="4d4d4e" />
+        </NavLink>
+        <NavLink onClick={() => setShowNav(false)}
+          exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
         <li>
           <a
             href="https://www.linkedin.com/in/kevinng3292/"
@@ -68,23 +81,18 @@ const Sidebar = () => (
           </a>
         </li>
         <li>
-          {/* <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon
-              icon={faSkype}
-              color="#4d4d4e"
-              className="anchor-icon"
-            />
-          </a> */}
+
         </li>
       </ul>
-      {/* <FontAwesomeIcon
+      <FontAwesomeIcon
         onClick={() => setShowNav(true)}
         icon={faBars}
         color="#ffd700"
         size="3x"
-        className='hamburger-icon' /> */}
-    
+        className='hamburger-icon' />
+
     </div>
-)
+  )
+}
 
 export default Sidebar
